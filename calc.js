863490b1450945209322e1b55daa207e0db1e06b
kazbak <bakarekazeem34@gmail.com>
2022-07-01 15:49:21 +0100

    Operations added

---
+++ calc.js
@@ -0,0 +1,22 @@
+var opr = prompt ("Please enter the Operator you want to perform \n e.g +, -, *,/ ")
+var num1 = parseInt (prompt ("First Number"))
+var num2 = parseInt (prompt ("Second Number"))
+
+if (opr == "+")
+{
+    prompt ("The result is " (num1 + num2))
+
+} else if (opr == "-")
+{   
+    prompt (num1-num2) 
+
+} else if (opr == "*")
+{   
+    prompt (num1*num2)
+
+} else if (opr == "/")
+{   
+    prompt (num1/num2)
+
+}
+
